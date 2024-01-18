e1404d1692dc53221694c388e46f9473bda7b640
skoanton <antonhakanssonn@gmail.com>
2024-01-18 16:03:18 +0100

    Done med skiten

--- dist/js/break.js
+++
@@ -1,8 +0,0 @@
-const buttonNoPause = document.getElementById("noPause");
-function addEventListners() {
-    buttonNoPause.addEventListener("click", goBack);
-}
-function goBack() {
-    console.log("Hello");
-}
-addEventListners();
