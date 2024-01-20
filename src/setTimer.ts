1f0118e28575b1826c4d737df5d86281eca962ca
Sebastian <selfishworm117@gmail.com>
2024-01-20 14:38:31 +0100

    set timer done, max 60 min, min 5 min.

---
+++ src/setTimer.ts
@@ -0,0 +1,20 @@
+const timeDisplay = document.getElementById("time-number");
+
+function increaseTimeFunc() {
+  let timeInt = parseInt(timeDisplay.textContent);
+  if (timeInt! < 60) {
+    timeInt += 5;
+    timeDisplay.textContent = `${timeInt}`;
+  }
+}
+
+function decraseTimeFunc() {
+  let timeInt = parseInt(timeDisplay.textContent);
+  if (timeInt > 5) {
+    timeInt -= 5;
+    timeDisplay.textContent = `${timeInt}`;
+  }
+}
+
+const increaseTime = document.querySelector(".increase-time").addEventListener("click", increaseTimeFunc);
+const decreaseTime = document.querySelector(".decrease-time").addEventListener("click", decraseTimeFunc);
