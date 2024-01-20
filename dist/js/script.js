2d2211e168c1c85f5856a34045e777fa798fb866
Ghayth Sabeaallil <ghaessesa58@gmail.com>
2024-01-20 14:56:04 +0100

    fix the clock and hadle with animation

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,2 +1,27 @@
-var min = document.getElementById("minHandle");
-console.log(min);
+//variables
+let sec = document.querySelector(".sec");
+let min = document.querySelector(".min");
+let btn = document.querySelector(".abrot-timer");
+let timerMin = 5;
+let iteration = 0;
+let angel = 6;
+function setTimer(timerMin) {
+    sec.style.animationIterationCount = timerMin.toString() + 1;
+    sec.addEventListener("animationiteration", () => {
+        min.style.transform = `rotate(${angel}deg)`;
+        angel = angel + 6;
+        iteration++;
+        if (iteration === timerMin) {
+            sec.style.animationPlayState = "paused";
+        }
+    });
+}
+function stop() {
+    if (sec.style.animationPlayState === "paused") {
+        sec.style.animationPlayState = "running";
+    }
+    else {
+        sec.style.animationPlayState = "paused";
+    }
+}
+setTimer(timerMin);
--- src/script.ts
+++ src/script.ts
@@ -1,2 +1,28 @@
-var min = document.getElementById("minHandle");
-console.log(min);
+//variables
+let sec = document.querySelector(".sec") as HTMLElement;
+let min = document.querySelector(".min") as HTMLElement;
+let btn = document.querySelector(".abrot-timer") as HTMLElement;
+
+let timerMin: number = 5;
+let iteration: number = 0;
+let angel: number = 6;
+
+function setTimer(timerMin: number) {
+  sec.style.animationIterationCount = timerMin.toString() + 1;
+  sec.addEventListener("animationiteration", () => {
+    min.style.transform = `rotate(${angel}deg)`;
+    angel = angel + 6;
+    iteration++;
+    if (iteration === timerMin) {
+      sec.style.animationPlayState = "paused";
+    }
+  });
+}
+function stop() {
+  if (sec.style.animationPlayState === "paused") {
+    sec.style.animationPlayState = "running";
+  } else {
+    sec.style.animationPlayState = "paused";
+  }
+}
+setTimer(timerMin);
