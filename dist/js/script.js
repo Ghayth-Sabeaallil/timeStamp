6bcfe7e79b7ed889126ca69e8254b77d15cdeb39
skoanton <antonhakanssonn@gmail.com>
2024-01-23 14:12:11 +0100

    ngt

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const easytimer_js_1 = require("easytimer.js");
 const timer = new easytimer_js_1.default();
-timer.start({ countdown: true, startValues: { seconds: 30 }, target: { seconds: 0 } });
+timer.start({ countdown: true, startValues: { seconds: 25 }, target: { seconds: 0 } });
 timer.addEventListener('secondsUpdated', function () {
     const timeValues = timer.getTimeValues();
     console.log('Remaining seconds:', timeValues.seconds);
--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,7 @@
 import Timer from "easytimer.js";
 const timer = new Timer();
 
-timer.start({countdown: true, startValues:{seconds: 30}, target:{seconds: 0}});
+timer.start({countdown: true, startValues:{seconds: 25}, target:{seconds: 0}});
 
 timer.addEventListener('secondsUpdated', function () {
     const timeValues = timer.getTimeValues();
