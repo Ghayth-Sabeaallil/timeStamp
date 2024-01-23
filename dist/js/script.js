038047330295360368c8893c7952fcdd83e4cb89
skoanton <antonhakanssonn@gmail.com>
2024-01-23 13:51:19 +0100

    la till easy timer console log

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,5 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const easytimer_js_1 = require("easytimer.js");
 const timer = new easytimer_js_1.default();
-console.log("Hello world");
-console.log("Hello world");
+timer.start({ countdown: true, startValues: { seconds: 30 }, target: { seconds: 0 } });
+timer.addEventListener('secondsUpdated', function () {
+    const timeValues = timer.getTimeValues();
+    console.log('Remaining seconds:', timeValues.seconds);
+});
--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,9 @@
 import Timer from "easytimer.js";
 const timer = new Timer();
 
+timer.start({countdown: true, startValues:{seconds: 30}, target:{seconds: 0}});
 
-console.log("Hello world");
-console.log("Hello world");
+timer.addEventListener('secondsUpdated', function () {
+    const timeValues = timer.getTimeValues();
+    console.log('Remaining seconds:', timeValues.seconds);
+});
