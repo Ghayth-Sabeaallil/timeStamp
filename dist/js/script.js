304947f83e471e449b6d88617ef0f51f1bf2230c
Ghayth Sabeaallil <ghaessesa58@gmail.com>
2024-01-23 13:08:03 +0100

    change the name of the svg and fix calAngle function

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,7 +2,7 @@
 let sec = document.querySelector(".sec");
 let min = document.querySelector(".min");
 let btn = document.querySelector(".abrot-timer");
-let timerMin = 7;
+let timerMin = 3;
 let iteration = 0;
 let angel = 6;
 function setTimer(timerMin) {
@@ -16,12 +16,41 @@ function setTimer(timerMin) {
         }
     });
 }
-function stop() {
-    if (sec.style.animationPlayState === "paused") {
-        sec.style.animationPlayState = "running";
+//calculate the angle
+function calAngle() {
+    let st = window.getComputedStyle(sec, null);
+    let tr = st.getPropertyValue("-webkit-transform") ||
+        st.getPropertyValue("-moz-transform") ||
+        st.getPropertyValue("-ms-transform") ||
+        st.getPropertyValue("-o-transform") ||
+        st.getPropertyValue("transform") ||
+        "fail...";
+    var values = tr.split("(")[1], values = values.split(")")[0];
+    var a = values.split(",")[0];
+    var b = values.split(",")[1];
+    var c = values.split(",")[2];
+    var d = values.split(",")[3];
+    var radians = Math.atan2(Number(b), Number(a));
+    if (radians < 0) {
+        radians += 2 * Math.PI;
     }
-    else {
-        sec.style.animationPlayState = "paused";
-    }
+    let angle = Math.round(radians * (180 / Math.PI));
+    return angle;
+}
+//show the Analog
+function showAnalog() {
+    let analog = document.querySelector(".analog-main");
+    analog.style.display = "flex";
+}
+//hide the Analog
+function hideAnalog() {
+    let analog = document.querySelector(".analog-main");
+    analog.style.display = "none";
+}
+function abrot() {
+    sec.style.animationPlayState = "paused";
+}
+function test() {
+    showAnalog();
 }
 setTimer(timerMin);
--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,9 @@
 //variables
-let sec = document.querySelector(".sec") as HTMLElement;
+let sec = document.querySelector(".sec") as any;
 let min = document.querySelector(".min") as HTMLElement;
 let btn = document.querySelector(".abrot-timer") as HTMLElement;
 
-let timerMin: number = 7;
+let timerMin: number = 3;
 let iteration: number = 0;
 let angel: number = 6;
 
@@ -19,11 +19,49 @@ function setTimer(timerMin: number) {
   });
 }
 
-function stop() {
-  if (sec.style.animationPlayState === "paused") {
-    sec.style.animationPlayState = "running";
-  } else {
-    sec.style.animationPlayState = "paused";
+//calculate the angle
+function calAngle(): number {
+  let st = window.getComputedStyle(sec, null);
+  let tr =
+    st.getPropertyValue("-webkit-transform") ||
+    st.getPropertyValue("-moz-transform") ||
+    st.getPropertyValue("-ms-transform") ||
+    st.getPropertyValue("-o-transform") ||
+    st.getPropertyValue("transform") ||
+    "fail...";
+
+  var values = tr.split("(")[1],
+    values = values.split(")")[0];
+
+  var a = values.split(",")[0];
+  var b = values.split(",")[1];
+  var c = values.split(",")[2];
+  var d = values.split(",")[3];
+
+  var radians = Math.atan2(Number(b), Number(a));
+  if (radians < 0) {
+    radians += 2 * Math.PI;
   }
+  let angle = Math.round(radians * (180 / Math.PI));
+  return angle;
+}
+
+//show the Analog
+function showAnalog() {
+  let analog = document.querySelector(".analog-main") as HTMLElement;
+  analog.style.display = "flex";
+}
+
+//hide the Analog
+function hideAnalog() {
+  let analog = document.querySelector(".analog-main") as HTMLElement;
+  analog.style.display = "none";
+}
+
+function abrot() {
+  sec.style.animationPlayState = "paused";
+}
+function test() {
+  showAnalog();
 }
 setTimer(timerMin);
