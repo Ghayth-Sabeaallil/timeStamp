dc094577ce4521c6ef8a40599498c2584f321889
Sebastian <selfishworm117@gmail.com>
2024-01-23 11:35:43 +0100

    va

--- dist/js/script.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const easytimer_js_1 = require("easytimer.js");
-const timer = new easytimer_js_1.default();
-timer.start();
-console.log(timer);
-console.log("Hello world");
-console.log("Hello world");
--- src/script.ts
+++
@@ -1,5 +0,0 @@
-import Timer from "easytimer.js";
-const timer = new Timer();
-
-console.log("Hello world");
-console.log("Hello world");
