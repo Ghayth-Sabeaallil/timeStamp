9e0621a02c7537dc6fc9dbc7a5635ac85f3411ba
ossian igelberg <ossian.igelberg@gmail.com>
2024-01-24 23:05:19 +0100

    renderad digital timer

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,13 +2,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const easytimer_js_1 = require("easytimer.js");
 const timer = new easytimer_js_1.default();
-timer.start({ countdown: true, startValues: { seconds: 30 }, target: { seconds: 0 } });
+/* const basicUsageDiv = document.getElementById('basicUsage') as HTMLDivElement;
+const resetBtn = document.querySelector('.resetBtn') as HTMLButtonElement; */
+timer.start({
+    countdown: false,
+    startValues: { minutes: 0, seconds: 0 },
+    target: { minutes: 10, seconds: 0 }
+});
 timer.addEventListener('secondsUpdated', function () {
     const timeValues = timer.getTimeValues();
-    console.log('Remaining seconds:', timeValues.seconds);
+    const formattedTime = formatTime(timeValues);
+    const basicUsageDiv = document.getElementById('basicUsage');
+    basicUsageDiv.innerText = formattedTime;
 });
-timer.start({ countdown: true, startValues: { seconds: 30 }, target: { seconds: 0 } });
-timer.addEventListener('secondsUpdated', function () {
-    const timeValues = timer.getTimeValues();
-    console.log('Remaining seconds:', timeValues.seconds);
-});
+function formatTime(timeValues) {
+    const minutes = String(timeValues.minutes).padStart(2, '0');
+    const seconds = String(timeValues.seconds).padStart(2, '0');
+    return `${minutes}:${seconds}`;
+}
--- src/script.ts
+++ src/script.ts
@@ -1,16 +1,24 @@
 import Timer from "easytimer.js";
 const timer = new Timer();
+/* const basicUsageDiv = document.getElementById('basicUsage') as HTMLDivElement;
+const resetBtn = document.querySelector('.resetBtn') as HTMLButtonElement; */
 
-timer.start({countdown: true, startValues:{seconds: 30}, target:{seconds: 0}});
+timer.start({
+    countdown: false,
+    startValues: { minutes: 0, seconds: 0 },
+    target: { minutes: 10, seconds: 0 }
+});
 
 timer.addEventListener('secondsUpdated', function () {
     const timeValues = timer.getTimeValues();
-    console.log('Remaining seconds:', timeValues.seconds);
+    const formattedTime = formatTime(timeValues);
+    
+    const basicUsageDiv = document.getElementById('basicUsage') as HTMLDivElement;
+    basicUsageDiv.innerText = formattedTime;
 });
 
-timer.start({countdown: true, startValues:{seconds: 30}, target:{seconds: 0}});
-
-timer.addEventListener('secondsUpdated', function () {
-    const timeValues = timer.getTimeValues();
-    console.log('Remaining seconds:', timeValues.seconds);
-});
+function formatTime(timeValues: { minutes: number, seconds: number }): string {
+    const minutes = String(timeValues.minutes).padStart(2, '0');
+    const seconds = String(timeValues.seconds).padStart(2, '0');
+    return `${minutes}:${seconds}`;
+}
