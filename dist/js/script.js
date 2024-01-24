8db628fdb6f7c7dcee8dd399ee289bceb7a7779c
ossian igelberg <ossian.igelberg@gmail.com>
2024-01-24 21:40:24 +0100

    fungerande-ish easytimer

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,9 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const easytimer_js_1 = require("easytimer.js");
 const timer = new easytimer_js_1.default();
-<<<<<<< HEAD
 timer.start({ countdown: true, startValues: { seconds: 30 }, target: { seconds: 0 } });
-=======
-console.log("Hello world");
-console.log("Hello world");
->>>>>>> origin/EasyTimer
+timer.addEventListener('secondsUpdated', function () {
+    const timeValues = timer.getTimeValues();
+    console.log('Remaining seconds:', timeValues.seconds);
+});
