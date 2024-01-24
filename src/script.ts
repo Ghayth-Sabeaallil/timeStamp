0c23c57b08864c9147f25138b41b580408910457
skoanton <antonhakanssonn@gmail.com>
2024-01-24 16:23:07 +0100

    merge med break-view-Anton2

--- src/script.ts
+++ src/script.ts
@@ -2,7 +2,6 @@ import Timer from "easytimer.js";
 
 const timer = new Timer();
 
-<<<<<<< HEAD
 timer.start({countdown: true, startValues:{minutes: 1, seconds: 5}, target:{seconds: 0}});
 
 timer.addEventListener('secondsUpdated', function () {
@@ -12,22 +11,4 @@ timer.addEventListener('secondsUpdated', function () {
 
 
 
-=======
-timer.start({countdown: true, startValues:{seconds: 30}, target:{seconds: 0}});
-
-timer.addEventListener('secondsUpdated', function () {
-    const timeValues = timer.getTimeValues();
-    console.log('Remaining seconds:', timeValues.seconds);
-});
-
-export function stopTimer(){
-    console.log("stop timer");
-    timer.stop();
- }
-
-export function resetTimer(){
-    timer.reset();
-}
-
->>>>>>> c21877237d88a5fd1947fefdaac9ea02389fae30
 export default timer;
