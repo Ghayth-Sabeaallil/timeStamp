5365e40a28a5852318065bbbf0262f94a81f654b
ossian igelberg <ossian.igelberg@gmail.com>
2024-01-24 21:46:23 +0100

    fungerande easytimer

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,3 +7,8 @@ timer.addEventListener('secondsUpdated', function () {
     const timeValues = timer.getTimeValues();
     console.log('Remaining seconds:', timeValues.seconds);
 });
+timer.start({ countdown: true, startValues: { seconds: 30 }, target: { seconds: 0 } });
+timer.addEventListener('secondsUpdated', function () {
+    const timeValues = timer.getTimeValues();
+    console.log('Remaining seconds:', timeValues.seconds);
+});
